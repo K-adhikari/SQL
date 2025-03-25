19ce55a5601cad6703bfcdb3a287a1a87014826c
K-adhikari <152343178+K-adhikari@users.noreply.github.com>
2025-03-25 15:28:37 -0400

    Create Module_1.sql

---
+++ Module_1.sql
@@ -0,0 +1,27 @@
+
+/*Question 1
+Retrieve all the records from the Employees table.*/
+
+select *
+from Employees
+
+
+/*Question 2
+Retrieve the FirstName, LastName, Birthdate, Address, City, and State from the Employees table.*/
+
+select
+FirstName,
+LastName,
+Birthdate,
+Address,
+City,
+State
+from Employees
+
+
+/*Question 3
+Retrieve all the columns from the Tracks table, but only return 20 rows.*/
+
+select *
+from Tracks
+limit 20;

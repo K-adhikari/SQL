a47d460ac4d8641a21bbc402666eaf5dfdc93348
K-adhikari <152343178+K-adhikari@users.noreply.github.com>
2025-11-11 08:56:17 -0500

    Update Module_1.sql

--- SQL_Basics_for_Data_Science/Module_1.sql
+++ SQL_Basics_for_Data_Science/Module_1.sql
@@ -6,14 +6,8 @@ FROM Employees
 
 -- Question 2. Retrieve the FirstName, LastName, Birthdate, Address, City, and State from the Employees table.
 
-select
-FirstName,
-LastName,
-Birthdate,
-Address,
-City,
-State
-from Employees
+SELECT FirstName, LastName, Birthdate, Address, City, State
+FROM Employees
 
 
 -- Question 3. Retrieve all the columns from the Tracks table, but only return 20 rows.
